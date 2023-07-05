e760c66a4d8cff0c3e310753974eb1c0febc0112
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2023-07-05 20:58:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ st.write("Hola")
 st.markdown("""
 <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/jmazcunan/testrepo/blob/main/test001.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Link</a>
 """,unsafe_allow_html=False)
+
+import streamlit.components.v1 as components
+
+# bootstrap 4 collapse example
+components.html("""<a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/jmazcunan/testrepo/blob/main/test001.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Link</a>
+""")
