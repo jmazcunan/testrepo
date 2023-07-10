7af006aeaafc6b43241a57909c65a6b5977e7c0c
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2023-07-10 10:49:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,24 @@ components.html("""<a href="intent://arvr.google.com/scene-viewer/1.0?file=https
 components.html("""<a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/jmazcunan/testrepo/main/copa_superliga2.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Copa SL</a>
 """)
 
+
+
+components.html("""<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+
+<title>Page Title</title>
+</head>
+<body>
+
+<model-viewer src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/sl_clean_0.2.glb"
+              ios-src="https://modelviewer.dev/shared-assets/models/Astronaut.usdz"
+              alt="A 3D model of an astronaut"
+              ar
+              auto-rotate
+              camera-controls></model-viewer>""")
+
+
+
+
 scripts_html = """<script type="module"
   src="https://unpkg.com/@google/model-viewer@3.1.1/dist/model-viewer.js">
 </script>
