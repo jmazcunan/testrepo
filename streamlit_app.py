2e794040538ab90403a9bcc414041cc6446299cd
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2023-07-10 09:51:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,30 @@ components.html("""<a href="intent://arvr.google.com/scene-viewer/1.0?file=https
 
 components.html("""<a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/jmazcunan/testrepo/main/copa_superliga2.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Copa SL</a>
 """)
+
+scripts_html = 
+"""
+<script type="module"
+  src="https://unpkg.com/@google/model-viewer@0.3.1/dist/model-viewer.js">
+</script>
+
+<script nomodule
+  src="https://unpkg.com/@google/model-viewer@0.3.1/dist/model-viewer-legacy.js">
+</script>
+"""
+
+ar_html = """components.html(scripts_html)
+
+<model-viewer src=”{}” 
+              ar
+              alt=”{}”>
+</model-viewer>
+"""
+
+
+components.html(scripts_html)
+
+
+components.html(ar_html.format("https://raw.githubusercontent.com/jmazcunan/testrepo/main/sl_clean_0.2.glb", "3D model")
+                
+
