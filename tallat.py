9f2f94ffd80db9af6d4955fd68afa65b799baff0
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2024-03-22 16:56:56 +0100

    Update tallat.py

--- tallat.py
+++ tallat.py
@@ -42,8 +42,8 @@ model-viewer {
 </style>
 
 
-<model-viewer src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/pinilla01.glb"
-              ios-src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/pinilla01.usdz"
+<model-viewer src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/pinilla02.glb"
+              ios-src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/pinilla02.usdz"
               alt="model viewer"
               ar
               auto-rotate
