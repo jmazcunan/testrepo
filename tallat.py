4da0a9bcb2e46addefe7dc7453855c33206a9f63
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2025-11-25 15:42:11 +0100

    Update tallat.py

--- tallat.py
+++ tallat.py
@@ -59,8 +59,8 @@ model-viewer {
 </style>
 
 
-<model-viewer src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/zazpiakbat_zip2.glb"
-              ios-src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/zazpiakbat_zip2.usdz"
+<model-viewer src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/zazpiakbat_exp2.glb"
+              ios-src="https://raw.githubusercontent.com/jmazcunan/testrepo/main/zazpiakbat_exp2.usdz"
               alt="model viewer"
               ar
               auto-rotate
